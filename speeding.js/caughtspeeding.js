e76f427a78cb432ec892247d9443106355d2fbbd
Erol Bayrasli <126ebayrasli@frhsd.com>
2025-02-13 14:03:15 -0500

    speeding script

---
+++ speeding.js/caughtspeeding.js
@@ -0,0 +1,15 @@
+function caughtSpeeding (x , y){
+    let isBirthday =x;
+    let speed = y;
+}
+    if(isBirthday){
+        speed = speed - 5;
+    }
+
+    if(speed <= 60){
+        return 0;}
+    
+        else if(speed <= 80 && speed >= 61){
+            return 1;}
+        else {
+            return 2;}
